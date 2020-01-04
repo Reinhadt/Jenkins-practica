5e4e91f0285d82839d2737bf94aff68a2818d436
Reinhadt <omavaloss@gmail.com>
2020-01-04 17:31:05 -0600

    Compilando desde development

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hola mundo desde java");
         System.out.println("Conectado a github por ngrok :)");
+        System.out.println("Compilando desde un branch nuevo :)");
     }
 }
