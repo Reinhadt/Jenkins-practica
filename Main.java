a323aea9f0a70ce7bc00b81070ce808bd1ca08ec
Reinhadt <omavaloss@gmail.com>
2020-01-04 17:14:09 -0600

    Segundo cambio agregado

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Hola mundo desde java");
-        System.out.println("Conectado a github");
+        System.out.println("Conectado a github por ngrok");
     }
 }
